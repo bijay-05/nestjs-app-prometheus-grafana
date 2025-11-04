35322415ae0113414dfc77fe9e6a3370bb404571
bijay-05 <hawkeyesniper69@gmail.com>
2025-11-04 20:57:55 +0545

    add cats module

---
+++ src/modules/cats/cats.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+
+@Injectable()
+export class CatsService {
+    constructor(){}
+
+    getCats(): string[] {
+        // throw new NotFoundException('Cats Not Found');
+        return ['cat1', 'cat2', 'cat3'];
+    }
+
+    createCat(): string {
+        // throw new NotFoundException('Cat Record cannot be created');
+        return 'Cat record created Successfully';
+    }
+}
