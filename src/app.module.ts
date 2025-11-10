114b52d250f8fc7b31789ede01a10258455d97cb
bijay-05 <hawkeyesniper69@gmail.com>
2025-11-10 22:40:03 +0545

    add mock auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
-import { Module, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerModule } from 'nestjs-pino';
 import { CommonModule } from './common/common.module';
 import { CatsModule } from './modules/cats/cats.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     CommonModule,
-    CatsModule
+    CatsModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,9 @@
+export class AuthService {
+  login(): string {
+    return "Login Successfull";
+  }
+
+  logout(): string {
+    return "Log out successfull";
+  }
+}
